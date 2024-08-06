fbc3afbe5e6d1c6391b13f278689dc258505515a
danischm <danischm@cisco.com>
2024-08-06 10:28:34 +0200

    Remove unnecessary dependencies

--- iac_validate/__init__.py
+++ iac_validate/__init__.py
@@ -2,11 +2,6 @@
 
 # Copyright: (c) 2022, Daniel Schmidt <danischm@cisco.com>
 
-try:
-    from importlib.metadata import PackageNotFoundError, version  # type: ignore
-except ImportError:  # pragma: no cover
-    from importlib_metadata import PackageNotFoundError, version
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
+from importlib.metadata import version  # type: ignore
+
+__version__ = version(__name__)
